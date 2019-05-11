0cf0fa055c6f25ae5871901ef9a73721e802b46e
Jorge Moreno <alterebro@gmail.com>
2019-05-11 14:42:33 +0200

    Colour Palette generator function

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,47 @@ import tinycolor from 'tinycolor2';
 import SVG from 'svgjs';
 import perlinNoise3d from 'perlin-noise-3d';
 
-const color = tinycolor('70693b');
-const draw = SVG('app').size(640, 480);
-      draw.viewbox({ x: 0, y: 0, width: 640, height: 480 })
-const bg = draw.rect(640, 480).fill( color.toHexString() );
-const noise = new perlinNoise3d();
-  let noiseStep = 0;
 
-for(let x = 0; x < 50; x++ ) {
-	let y = noise.get(noiseStep) * 480;
-    draw.circle(5).cx( Math.round((640/50)*x) ).cy( Math.floor(y) ).fill('#fff');
-	noiseStep += 0.05;
+function colorPalette(color) {
+
+    const _default = '#30609f';
+    const self = (tinycolor(color).isValid())
+        ? tinycolor(color).toHexString()
+        : _default;
+
+    const analogous = (function() {
+
+        let _analogous = tinycolor(self).analogous(7, 30);
+            _analogous.shift();
+            _analogous = _analogous.map((c) => c.toHex() );
+
+        // Make them always go Light to Dark
+        if ( tinycolor(_analogous[_analogous.length-1]).getLuminance() > tinycolor(_analogous[0]).getLuminance() ) {
+            _analogous.reverse();
+        }
+
+        // Darken the darks and Lighten the lights
+        _analogous = _analogous.map((el, i, arr) => {
+
+            let _i = ((i+1) - (arr.length/2)) * 15;
+            // let _i = (i - (arr.length/2)) * 15;
+
+            let _elem = tinycolor(el);
+            let _el = _elem;
+
+                if ( _i < 0 ) { _el = _elem.lighten( Math.abs(_i) ) }
+                if ( _i > 0 ) { _el = _elem.darken( Math.abs(_i) ) }
+
+            return _el.desaturate(30).toHexString();
+        });
+
+        return _analogous
+
+    })();
+
+    const complementary = tinycolor(self).complement().brighten(40).desaturate(40).toHexString();
+
+    return { self, analogous, complementary }
 }
+
+console.log( colorPalette('234') );
